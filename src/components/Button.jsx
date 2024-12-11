035e028f938ee84fb53aeb23a00026e93cac28fb
rowdy-byte <rowdy.verbeek@outlook.com>
2024-12-11 19:05:27 +0100

    .

---
+++ src/components/Button.jsx
@@ -0,0 +1,8 @@
+export default function Button({ type, children }) {
+  return (
+    <button className={`btn ${type === "secondary" ? "btn--secondary" : ""}`}>
+      {" "}
+      {children}{" "}
+    </button>
+  );
+}
--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,3 +1,34 @@
+const initialItems = [
+  {
+    name: "good mood",
+    packed: false,
+  },
+  {
+    name: "passport",
+    packed: false,
+  },
+  {
+    name: "phone charger",
+    packed: false,
+  },
+];
+
 export default function ItemList() {
-  return <div>ItemList</div>;
+  return (
+    <ul>
+      {initialItems.map((item) => {
+        return <Item key={item.name} item={item} />;
+      })}
+    </ul>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li className="item">
+      <label>
+        <input type="checkbox" /> {item.name}
+      </label>
+    </li>
+  );
 }
